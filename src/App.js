a1a1fd8da10cae6bbd90a06645de432cf10562cc
Somesh <Somesh.4543@gmail.com>
2022-04-15 20:11:07 +0530

    Links

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
+import Layout from "./components/layout/Layout";
 import AllQuotes from "./Routes/AllQuotes";
 import NewQuotes from "./Routes/NewQuotes";
 import QuoteDetail from "./Routes/QuoteDetail";
 
 function App() {
   return (
-    <div>
-      <Switch>
-        <Route path="/quotes" exact>
-          <AllQuotes />
-        </Route>
-        <Route path="/quotes/quoteId">
-          <QuoteDetail />
-        </Route>
-        <Route path="/new-quote">
-          <NewQuotes />
-        </Route>
-      </Switch>
-    </div>
+      <Layout>
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/quotes" />
+          </Route>
+          <Route path="/quotes" exact>
+            <AllQuotes />
+          </Route>
+          <Route path="/quotes/:quoteId">
+            <QuoteDetail />
+          </Route>
+          <Route path="/new-quote">
+            <NewQuotes />
+          </Route>
+        </Switch>
+      </Layout>
   );
 }
 
--- src/Routes/AllQuotes.js
+++ src/Routes/AllQuotes.js
@@ -1,7 +1,7 @@
 const AllQuotes = () => {
-    return (
-        <h1>All Quotes Page</h1>
-    );
+  return (
+      <h1>All Quotes Page</h1>
+  );
 };
 
 export default AllQuotes;
