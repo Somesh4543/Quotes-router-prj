f39e20bb069920c55940902a3f08f75d7a53ad53
Somesh <Somesh.4543@gmail.com>
2022-04-15 19:00:39 +0530

    Switch

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import { Route, Switch } from "react-router-dom";
+import AllQuotes from "./Routes/AllQuotes";
+import NewQuotes from "./Routes/NewQuotes";
+import QuoteDetail from "./Routes/QuoteDetail";
+
 function App() {
   return (
     <div>
-
+      <Switch>
+        <Route path="/quotes" exact>
+          <AllQuotes />
+        </Route>
+        <Route path="/quotes/quoteId">
+          <QuoteDetail />
+        </Route>
+        <Route path="/new-quote">
+          <NewQuotes />
+        </Route>
+      </Switch>
     </div>
   );
 }
