d70c1228f3a8f3cddc12d6a132cc4cdb9ffe2bc0
Somesh4543 <Somesh.4543@gmail.com>
2022-05-06 19:07:06 +0530

    new

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Switch, Redirect } from "react-router-dom";
 import Layout from "./components/layout/Layout";
 import AllQuotes from "./Routes/AllQuotes";
 import NewQuotes from "./Routes/NewQuotes";
+import NotFound from "./Routes/NotFound";
 import QuoteDetail from "./Routes/QuoteDetail";
 
 function App() {
@@ -19,6 +20,9 @@ function App() {
           </Route>
           <Route path="/new-quote">
             <NewQuotes />
+          </Route>
+          <Route path='*'>
+            <NotFound />
           </Route>
         </Switch>
       </Layout>
