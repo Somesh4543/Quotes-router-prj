215d956e97a30069836da2a018125570286a2901
Somesh4543 <Somesh.4543@gmail.com>
2022-05-06 19:06:42 +0530

    Ascending

--- src/Routes/AllQuotes.js
+++ src/Routes/AllQuotes.js
@@ -1,7 +1,12 @@
+import QuoteList from '../components/quotes/QuoteList';
+
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Max", text: "This is done!" },
+  { id: "q2", author: "Mo", text: "This is great!" },
+];
+
 const AllQuotes = () => {
-  return (
-      <h1>All Quotes Page</h1>
-  );
+  return <QuoteList quotes={DUMMY_QUOTES}/>
 };
 
 export default AllQuotes;
